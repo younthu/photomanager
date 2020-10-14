62f5aadda9edf5853356ec9ac4a204a0417eb8b7
Zhiyong Yang <evil_eve_live@outlook.com>
2020-10-14 16:59:36 +0800

    sort and pretty print files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+import pprint
 from pathlib import Path
 
 
@@ -8,10 +9,16 @@ def walk_folder(folder):
     types = ["**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.gif"]
     return [(file, file.stat().st_size) for t in types for file in p.glob(t) ]
 
+def pretty_print(images):
+    pp = pprint.PrettyPrinter(depth=6)
+    # sort
+    sorted_images = sorted(images, key=lambda tup: tup[1])
+    pp.pprint(sorted_images)
+    
 @click.command()
 @click.option("--path", default=".", prompt="the folder to scan")
 def photomanager(path):
-    print( walk_folder(path))
+    pretty_print( walk_folder(path))
 
 if __name__ == "__main__":
     photomanager()
