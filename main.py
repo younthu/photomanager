6d4123aa450085893b60c72309aba44db217cc55
Zhiyong Yang <evil_eve_live@outlook.com>
2020-10-14 13:34:27 +0800

    get file size

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def walk_folder(folder):
     p = Path(folder)
     types = ["**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.gif"]
-    return [x for t in types for x in p.glob(t) ]
+    return [(file, file.stat().st_size) for t in types for file in p.glob(t) ]
 
 @click.command()
 @click.option("--path", default=".", prompt="the folder to scan")
